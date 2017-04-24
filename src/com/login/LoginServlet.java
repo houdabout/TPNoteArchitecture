dce0d347dfbac867a8825ecbe6bd92e392e891a2
DESKTOP-OEDPMIF\Houda BOUTBIB <houda.boutbib@gmail.com>
2017-04-24 22:32:12 +0200

    login page

---
+++ src/com/login/LoginServlet.java
@@ -0,0 +1,59 @@
+package com.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String uname=request.getParameter("uname");
+		String pass = request.getParameter("pass");
+		
+		
+		if(uname.equals("houda")&&pass.equals("bout"))
+
+		{
+			response.sendRedirect("welcome.jsp");
+		}
+		}
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
